460c83467ed3a4241f62461874001a0252b1131b
one-nora <193192971+one-nora@users.noreply.github.com>
2026-04-06 12:24:35 -0300

    build: Rework .csproj and build events

--- WK_Lib.cs
+++ WK_Lib.cs
@@ -11,15 +11,19 @@ namespace WKLib;
 /// <summary>
 /// Main Entry point of the whole Library
 /// </summary>
-[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+[BepInPlugin(GUID, NAME, VERSION)]
 public class WkLib : BaseUnityPlugin
 {
+    public const string GUID = "com.monksilly.WKLib";
+    public const string NAME = "WKLib";
+    public const string VERSION = "0.0.2";
+    
     private void Awake()
     {
         // Initialize Logger
         WKLog.Initialize(Logger);
         
-        WKLog.Info($"Plugin {MyPluginInfo.PLUGIN_NAME} v{MyPluginInfo.PLUGIN_VERSION} is loaded!");
+        WKLog.Info($"Plugin {NAME} v{VERSION} is loaded!");
         
         // Initialize UI System
         UIManager.TryInitialize();
@@ -34,7 +38,7 @@ public class WkLib : BaseUnityPlugin
         var versionText = GameObject.Find("Canvas - Main Menu/Main Menu/Version Text").GetComponent<TextMeshProUGUI>();
         if (versionText is null) return;
 
-        versionText.text += $" (wklib-{MyPluginInfo.PLUGIN_VERSION}) ({BepInEx.Bootstrap.Chainloader.PluginInfos.Count} Mods)";
+        versionText.text += $" (wklib-{VERSION}) ({BepInEx.Bootstrap.Chainloader.PluginInfos.Count} Mods)";
     }
     
 }
