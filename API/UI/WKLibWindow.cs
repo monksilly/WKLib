43ce246331a8b7509ab1dac1be2928ac8fc7b2d9
one-nora <193192971+one-nora@users.noreply.github.com>
2026-04-06 16:32:10 -0300

    feat: Add WKLibWindow

--- API/UI/WKLibWindow.cs
+++ API/UI/WKLibWindow.cs
@@ -1,6 +1,14 @@
+using Imui.Core;
+using JetBrains.Annotations;
+
 namespace WKLib.API.UI;
 
-public class WKLibWindow
+[PublicAPI]
+public abstract class WKLibWindow
 {
-    
+    public bool isOpen = false;
+    public bool isMainConfigWindow = false;
+
+    public abstract void Draw(ImGui gui, bool isRootPanelOpen);
+    public abstract void HandleInput(ImGui gui);
 }
