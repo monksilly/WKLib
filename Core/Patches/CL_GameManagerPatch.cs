18b114a37a42d37e672167f6dc106a251da57a8e
one-nora <193192971+one-nora@users.noreply.github.com>
2026-04-07 13:15:36 -0300

    feat(Patches): Add CL_GameManager and InputManager patches

---
+++ Core/Patches/CL_GameManagerPatch.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using WKLib.Core.Attributes;
+using WKLib.Core.UI;
+using static WKLib.Core.Config.ConfigManager;
+
+namespace WKLib.Core.Patches;
+
+[PatchOnEntry]
+[HarmonyPatch]
+internal static class CL_GameManagerPatch
+{
+    [HarmonyPatch(typeof(CL_GameManager), nameof(CL_GameManager.UnPause)), HarmonyPostfix]
+    private static void CL_GameManager_UnPause(CL_GameManager __instance)
+    {
+        if (RootPanel.Instance == null)
+            return;
+
+        if (!AutoCloseOverlay
+            || !RootPanel.Instance.IsOpen
+            || __instance.isPaused)
+            return;
+
+        RootPanel.Instance.IsOpen = false;
+    }
+}
---
+++ Core/Patches/InputManagerPatch.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using ImuiBepInEx.API;
+using WKLib.Core.Attributes;
+using WKLib.Core.UI;
+
+namespace WKLib.Core.Patches;
+
+[PatchOnEntry]
+[HarmonyPatch]
+internal static class InputManagerPatch
+{
+    [HarmonyPatch(typeof(InputManager), nameof(InputManager.Start)), HarmonyPostfix]
+    private static void InputManager_Start(InputManager __instance)
+    {
+        if (RootPanel.Instance != null)
+            return;
+
+        ImuiBepInExAPI.CreateImuiPanel<RootPanel>();
+    }
+}
