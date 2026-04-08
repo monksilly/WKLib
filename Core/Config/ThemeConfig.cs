f0610147ba58ca60e64914d8f18d35a9f72f824f
one-nora <193192971+one-nora@users.noreply.github.com>
2026-04-08 16:57:39 -0300

    feat(Theme): Add IEquatable and ICloneable to Config

--- Core/Config/ThemeConfig.cs
+++ Core/Config/ThemeConfig.cs
@@ -1,9 +1,38 @@
+using System;
 using UnityEngine;
 
 namespace WKLib.Core.Config;
 
-public class ThemeConfig
+internal class ThemeConfig : IEquatable<ThemeConfig>, ICloneable
 {
     public bool HighContrast = false;
     public Color AccentColor = new Color(0.05f, 0.45f, 0.75f, 1f);
+    
+    public ThemeConfig() { }
+
+    public ThemeConfig(bool highContrast, Color accentColor)
+    {
+        HighContrast = highContrast;
+        AccentColor = accentColor;
+    }
+    
+    public object Clone()
+    {
+        return new ThemeConfig(HighContrast, AccentColor);
+    }
+    
+    public override int GetHashCode()
+    {
+        return HighContrast.GetHashCode() + AccentColor.GetHashCode();
+    }
+    
+    public bool Equals(ThemeConfig other)
+    {
+        if (other is null)
+            return false;
+
+        return HighContrast == other.HighContrast && AccentColor == other.AccentColor;
+    }
+    
+    public override bool Equals(object obj) => Equals(obj as ThemeConfig);
 }
